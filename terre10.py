3c399989017fe452dac0450469241160c2f1eaad
Jérôme Theytaz <jerome.theytaz@gmail.com>
2023-02-28 16:21:40 -0400

    Add terre10.py

---
+++ terre10.py
@@ -0,0 +1,28 @@
+# Import module
+import sys
+
+# Verify Errors
+try:
+    number = int(sys.argv[1])
+except ValueError:
+    print("Please enter a number.")
+    sys.exit()
+except IndexError:
+    print("Invalid input. No provided number.")
+    sys.exit()
+
+# Manage more arguments
+if len(sys.argv) > 2:
+    print("Invalid input. Please provide only one integer.")
+    sys.exit()
+
+# Loop to know if prime number
+if number > 1:
+    for i in range(2, int(number/2)+1):
+        if (number % i) == 0:
+            print("It is not a prime number.")
+            break
+    else:
+        print("It is a prime number.")
+else:
+    print("It is not a prime number.")
