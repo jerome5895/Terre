9a004fac9da21c195d9bddcf2e4e7303c15a3593
Jérôme Theytaz <jerome.theytaz@gmail.com>
2023-02-28 16:18:27 -0400

    Add terre02.py

---
+++ terre02.py
@@ -0,0 +1,9 @@
+# import module
+import sys
+
+# convert all arguments
+argument = sys.argv[::]
+
+# program that displays the arguments that its receives, line by line
+for arg in argument:
+    print(arg)
