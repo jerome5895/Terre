2446502cde4dcbf514a1d7dd0d3ea9d8ce059214
Jérôme Theytaz <jerome.theytaz@gmail.com>
2023-02-28 19:06:55 -0400

    Add terre15.py

---
+++ terre15.py
@@ -0,0 +1,8 @@
+my_sentence = "J'ai terminé l'épreuve de la Terre. C'était souvent difficile:("
+
+my_green_points = "Table ASCII", "Syntaxe", "Conditions", "Github"
+
+my_less_green_points = "Loop", "More developed algorithmics", "New functions"
+
+# Print out Terre over
+print("Super!", my_sentence, "Je garde cependant mon état d'esprit, je ne m'attendais pas à que ce soit facile.", "Mes points forts:", my_green_points, "Mes points faibles:", my_less_green_points)
