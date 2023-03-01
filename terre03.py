8e1d93a7252cd49084fb6cd66718c17df6226257
Jérôme Theytaz <jerome.theytaz@gmail.com>
2023-03-01 14:10:01 -0400

    Added 'print()' and modify comments, terre03.py

--- terre03.py
+++ terre03.py
@@ -1,12 +1,14 @@
-# import module to extract argument
+# Import module
 import sys
 
-# convert variable to a number
+# Convert variable to a number
 letter_from = ord(sys.argv[1])
 
-# establishment the rest of the alphabet
+# Establishment the rest of the alphabet
 alphabet = range((letter_from), (123))
  
-# a programm that displays the alphabet from the letter given as an argument in lowercase letters followed by a newline
+# Programm that displays the alphabet from the letter given as an argument in lowercase letters followed by a newline
 for letter in alphabet:
     print(chr(letter), end="")
+
+print()
