c6f4c7243c1b880eaa29ce628fa1d35dc9eb57cd
Jérôme Theytaz <jerome.theytaz@gmail.com>
2023-02-28 20:13:39 -0400

    Add terre01.py

---
+++ terre01.py
@@ -0,0 +1,6 @@
+# Path to the file
+Path = ["C", "Users", "Jerome", "Documents", "Terre", "Lessons", "terre01.1.py"]
+
+# A program that displays the name of its file
+for title in Path[-1]:
+  print((title), end="")
