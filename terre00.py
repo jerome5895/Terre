9b824c74c5b6b2a0e08412609772fd5f25d26d9f
Jérôme Theytaz <jerome.theytaz@gmail.com>
2023-03-01 13:54:04 -0400

    Added 'print()' to have a line break, terre00.py

--- terre00.py
+++ terre00.py
@@ -1,10 +1,5 @@
-# Displays the lesson of the exercice     
-def notes():
-    print("There, the lesson is: Know the ASCII table, how to use that in a loop:")
-
-notes()
-
 # Displays the alphabet in lowercase letters
 for lettre in range(97,123,1):
     print(chr(lettre), end="")
 
+print()
