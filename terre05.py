ae62567d1fd230e74c08ca79514ab646f052b3b0
Jérôme Theytaz <jerome.theytaz@gmail.com>
2023-03-01 15:20:01 -0400

    Edit comments, terre05.py

--- terre05.py
+++ terre05.py
@@ -1,7 +1,7 @@
-# import module
+# Import module
 import sys
 
-# verify Errors
+# Verify Errors
 try:
     number_1 = int(sys.argv[1])
     number_2 = int(sys.argv[2])
@@ -16,17 +16,18 @@ except IndexError:
     print("One integer at least is missing")
     sys.exit()
 
-# functions for result and remainder
+# Functions result and remainder
 def result(a, b):
     return a // b
 def remainder(a, b):
     return a % b
 
-# verify divider size
+# Verify divider size
 if number_2 > number_1:
     print("Your divider is bigger than your dividend.")
 
-# print out the result and the remainder
+# Print out the result and the remainder
 print("The result is:", result(number_1, number_2))    
 print("The remainder is:", remainder(number_1, number_2))
+
 
