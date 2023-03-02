9303cd3f3358ca4c23fb52f3947acffb8044fefa
Jérôme Theytaz <jerome.theytaz@gmail.com>
2023-03-02 16:35:59 -0400

    Establishing three functions, modify comments, terre05.py

--- terre05.py
+++ terre05.py
@@ -1,33 +1,40 @@
-# Import module
 import sys
 
-# Verify Errors
+# Function that manages errors
+def try_except(nbr1, nbr2):
+    try:
+        nbr1 = int(nbr1)
+        nbr2 = int(nbr2)
+    except ZeroDivisionError:
+        print("Invalid input. One of your numbers is zero.")
+        sys.exit()
+    except ValueError:
+        print("Invalid input. Please enter two integers.")
+        sys.exit()
+    except IndexError:
+        print("Invalid input. One integer at least is missing")
+        sys.exit()
+    return nbr1, nbr2
+
+# Function that returns result and remainder
+def result_remainder(nbr1, nbr2):
+    return nbr1 // nbr2, nbr1 % nbr2
+
+# Function that returns if divider too big
+def divider_size(nbr1, nbr2):
+    if nbr2 > nbr1:
+        print("Invalid input. Your divider is bigger than your dividend.")
+        sys.exit()
+
+# Unpack the tuple returned by try_except
 try:
-    number_1 = int(sys.argv[1])
-    number_2 = int(sys.argv[2])
-    result = (number_1 // number_2)
-except ZeroDivisionError:
-    print("Invalid input. One of your numbers is zero.")
-    sys.exit()
-except ValueError:
-    print("Invalid input. Please enter two integers.")
-    sys.exit()
+    nbr1, nbr2 = try_except(sys.argv[1], sys.argv[2])
 except IndexError:
-    print("One integer at least is missing")
+    print("Invalid input. One integer at least is missing.")
     sys.exit()
 
-# Functions result and remainder
-def result(a, b):
-    return a // b
-def remainder(a, b):
-    return a % b
-
-# Verify divider size
-if number_2 > number_1:
-    print("Your divider is bigger than your dividend.")
-
-# Program that print out the result and the remainder in argument
-print("The result is:", result(number_1, number_2))    
-print("The remainder is:", remainder(number_1, number_2))
-
-
+# Call function to print out result and remainder expected
+divider_size(nbr1, nbr2)
+result, remainder = result_remainder(nbr1, nbr2)
+print(f"Result: {result}")
+print(f"Remainder: {remainder}")
