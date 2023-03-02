99ee5170b55431ce34994471d1504a8cb4463c17
Jérôme Theytaz <jerome.theytaz@gmail.com>
2023-03-02 15:06:34 -0400

    Establishing function 'alphabet from', modify comments, terre03.py

--- terre03.py
+++ terre03.py
@@ -1,14 +1,11 @@
-# Import module
-import sys
+# Function that returns alphabet from given letter in the prompt
+def alphabet_from_():
+    import sys
+    letter_from = ord(sys.argv[1])
+    alphabet = range((letter_from), (123))
+    for letter in alphabet:
+        print(chr(letter), end="")
+    print(("\n"), end="")
 
-# Convert variable to a number
-letter_from = ord(sys.argv[1])
-
-# Establishment the rest of the alphabet
-alphabet = range((letter_from), (123))
- 
-# Programm that displays the alphabet from the letter given as an argument in lowercase letters followed by a newline
-for letter in alphabet:
-    print(chr(letter), end="")
-
-print()
+# Call function
+alphabet_from_()
