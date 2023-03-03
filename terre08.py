599dfecc87ea81db1bff84d6a3266b7e4455a55a
Jérôme Theytaz <jerome.theytaz@gmail.com>
2023-03-03 11:45:45 -0400

    Added three functions and edited comments, terre08.py

--- terre08.py
+++ terre08.py
@@ -1,27 +1,36 @@
-# Import module
 import sys
 
-# Verify Errors
+# Function to manage errors
+def try_except(nbr1, nbr2):
+    try:
+        nbr1 = int(nbr1)
+        nbr2 = int(nbr2)
+    except ValueError:
+        print("Invalid input. Please provide only integers.")
+        sys.exit()
+    except IndexError:
+        print("Invalid input. One integer at lest is missing.")
+        sys.exit()
+    return nbr1, nbr2
+
+# Function to calcul power
+def power(nbr1, nbr2):
+    return nbr1 ** nbr2
+
+# Unpack the tuple returned by the try_except
 try:
-    number_1 = int(sys.argv[1])
-    number_2 = int(sys.argv[2])
-    result = number_1 ** number_2
+    nbr1, nbr2 = try_except(sys.argv[1], sys.argv[2])
 except IndexError:
-    print("Invalid input. One integer at lest is missing.")
-    sys.exit()
-except ValueError:
-    print("Invalid input. Please provide only integers.")
+    print("Invalid input. One integer at least is missing.")
     sys.exit()
 
-# Function power
-def power(a, b):
-    return a ** b
+# Function that checks if exponent is negative
+def verify_negative_exponent(nbr2):
+    if int(nbr2) < 0:
+        print("Invalid input. Negative number.")
+        sys.exit()
 
-# Condition negative number
-if number_2 < 0:
-    print("Invalid input. Negative number.")
-    sys.exit()
-
-# Print out the result of power
-print("The result is:", power(number_1, number_2))
-
+# Call function to checks exponent and print out result
+verify_negative_exponent(nbr2)
+result = power(nbr1, nbr2)
+print(f"The result is: {result}")
