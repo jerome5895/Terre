0990f9cd2615a98cebcddb6d43c7ae6dfc969d7c
Jérôme Theytaz <jerome.theytaz@gmail.com>
2023-03-03 12:58:39 -0400

    Added four functions and edited comments, terre09.py

--- terre09.py
+++ terre09.py
@@ -1,30 +1,43 @@
-# Import module
 import sys
 
-# Verify Errors
+# Function to manage errors
+def try_except(nbr):
+    try:
+        nbr = int(nbr)
+    except ValueError:
+        print("Invalid input. Please provide only integer.")
+        sys.exit()
+    except IndexError:
+        print("Invalid input. No number provided.")
+        sys.exit()
+    return nbr
+
+# Function to calcul the square root
+def square_root(nbr):
+    return int(nbr) ** 0.5
+
+# Function to verify if negative exponent
+def verify_negative_number(nbr):
+    if int(nbr) < 0:
+        print("Invalid input. Negative number.")
+        sys.exit()
+
+# Unpack the tuple receved by the try_except
 try:
-    number = int(sys.argv[1])
-    result = number ** 0.5
-except ValueError:
-    print("Invalid input. Please provide only integer.")
-    sys.exit()
+    nbr = sys.argv[1]
 except IndexError:
-    print("Invalid input. No number provided.")
+    print("Invalid input. no number provided.")
     sys.exit()
 
-# Function square root
-def square_root(a):
-    return a ** 0.5
+# Function to check if more than one argument
+def if_multi_nbr(length):
+    if length > 2:
+        print("Invalid input, please provide only one integer.")
+        sys.exit()
 
-# Condition negative argument
-if number < 0:
-    print("Invalid input. Negative number.")
-    sys.exit()
-
-# Condition for more arguments
-if len(sys.argv) > 2:
-    print("Invalid input, please provide only one integer.")
-    sys.exit()
-
-# Print out square root
-print("The square root is:", square_root(number))
+# Call function to verify length, try_except, negative number and to print out 
+length = len(sys.argv)
+try_except(nbr)
+if_multi_nbr(length)
+verify_negative_number(nbr)
+print(f"The square root is: {square_root(nbr)}")
