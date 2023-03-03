4f8ca10e0dfd9dcccc5c60324bfb59692b9a2dc7
Jérôme Theytaz <jerome.theytaz@gmail.com>
2023-03-02 21:42:04 -0400

    Added three functions and modify comments, terre07.py

--- terre07.py
+++ terre07.py
@@ -1,27 +1,35 @@
-# Import module
 import sys
 
+# Function to manage errors
+def try_except(argument):
+    try:
+        argument = sys.argv[1]
+    except IndexError:
+        print("There is not any input.")
+        sys.exit()
+    return argument
 
-# Verify Errors
-try:
-    argument = sys.argv[1]
-except IndexError:
-    print("There is not any input.")
+argument = try_except(sys.argv)
+
+# Function to check if argument is only numbers
+def is_only_numbers(argument):
+    return argument.isdigit()
+
+# Function to have the length of the argument
+def length_argument(argument):
+    length = 0
+    for char in str(argument):
+        length = length + 1
+    return str(length)
+
+# Checks if only numbers or more than one argument
+if is_only_numbers(argument):
+    print("Invalid input. Please do not enter only numbers.")
     sys.exit()
-
-# Verify if only numbers
-if argument.isdigit():
-    print("Invalid input")
-    sys.exit()
-
-# Invalid if more than one argument
 if len(sys.argv) > 2:
-    print("Invalid input")
+    print("Invalid input. Please enter only 'one argument'.")
     sys.exit()
 
-# Print out the lenght of the first argument
-lenght = 0
-for calcul in argument:
-    lenght = lenght + 1
-
-print(lenght)
+# Call function to check errors and function to print the length
+length = try_except(argument)
+print(length_argument(argument))
