a40a53371e43d7974678d331f6cec32d1b84c848
Jérôme Theytaz <jerome.theytaz@gmail.com>
2023-03-03 21:16:06 -0400

    Added two functions and edit comments, terre14.py

--- terre14.py
+++ terre14.py
@@ -1,21 +1,33 @@
 import sys
 
-# Test line code in the variable "Integers", to manage Errors
-try:
-    integers = [int(integer) for integer in sys.argv[1:]]   
-except ValueError:
-    print("Please enter a list of integers.")
-    sys.exit()
+# Function to manage errors
+def try_except():
+    if len(sys.argv) < 3:
+        print("Invalid input. Please provide at least two integers.")
+        sys.exit()
+    try:
+        integers = [int(integer) for integer in sys.argv[1:]]
+        return integers
+    except ValueError:
+        print("Invalid input. Please enter a list of integers.")
+        sys.exit()
+    
+# Call function to manage errors
+integers = try_except()
 
-# To sort "Integers" to "sorted" or "not sorted"
-sorted = True
-for i in range(len(integers) - 1):
-    if integers[i] > integers[i+1]:
-        sorted = False
-        break
+# Function to know if list of integers is sorted
+def if_integers_sorted(integers):
+    sorted = True
+    for i in range(len(integers) - 1):
+        if integers[i] > integers[i+1]:
+            sorted = False
+            break
+    return sorted
 
-# Two possible output 
-if sorted:
+is_sorted = if_integers_sorted(integers)
+
+# Print out the exit of function `sort_integers`
+if is_sorted:
     print("Your list is sorted!")
 else:
     print("Your list is not sorted!")
