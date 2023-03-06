73a0d3188c49c59622b471c26676ec770ec30389
Jérôme Theytaz <jerome.theytaz@gmail.com>
2023-03-06 10:46:41 -0400

    Changed '.split result', terre12.py

--- terre12.py
+++ terre12.py
@@ -1,33 +1,60 @@
-# Import module
 import sys
 
-# To try Errors
-try:
-    argument = sys.argv[1]
-    hours, minutes = argument[:-2].split(":")
-except ValueError:
-    print("Invalid input. Please try again.")
-    sys.exit()
-except IndexError:
-    print("Invalid input. Please try again.")
-    sys.exit()
+# Function to manage errors
+def try_except():
+    try:
+        argument = sys.argv[1]
+    except ValueError:
+        print("Invalid input. Please do not enter any letters.")
+        sys.exit()
+    except IndexError:
+        print("Invalid input. There is no input.")
+        sys.exit()
+    return argument
 
-# To convert 12h format to 24h format
-if argument[-2:] == "PM":
-    if hours != "12":
-        hours = str(int(hours) + 12)
-    else:             
-        hours = "12"
-else:
-    if hours == "12":
-        hours = "00"
+argument = try_except()
 
-# Manage errors argument not full
-if argument[-2:] != "AM" or argument[-2:] != "PM":
-        print("Invalid input. Please try again.")
+# Separate hours and minutes from argument    
+hours, minutes = argument[:-2].split(":")
+
+hours = int(hours)
+minutes = int(minutes)
+
+# Function to convert 12-hours to 24-hours format
+def format_12_to_24(hours):
+    if argument[-2:] == "PM":
+        if hours != 12:
+            hours += 12
+        else:             
+            hours = 12
+    else:
+        if hours == 12:
+            hours = 0
+    return hours
+
+# Function to verify if minutes are provided
+def if_not_minute():
+    if not minutes:
+        print("Invalid input. Please provide a valide time in the format 'HH:MM AM/PM'.")
         sys.exit()
 
-time_24h_format = f"{hours}:{minutes}"
+# Function to verify if "AM/PM" is provided
+def if_not_am_pm():
+    if argument[-2:] not in ["AM", "PM"]:
+            print("Invalid input. Please try again.")
+            sys.exit()
 
+# Function to verify if ":" is provided
+def if_double_points():
+    if ":" not in argument:
+        print("Invalid input. Please provide a valid time in the format 'HH:MM AM/PM'.")
+        sys.exit()
 
+if_double_points()
+if_not_minute()
+if_not_am_pm()
+hours = format_12_to_24(hours)
+
+# Print out input in the 24-h format
+time_24h_format = f"{hours:02}:{minutes:02}"
 print(time_24h_format)
